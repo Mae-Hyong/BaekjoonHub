64917e28bdb5b391de4ab07473ed06688f1d3275
Hyeo_Ni__ <148944993+Mae-Hyong@users.noreply.github.com>
2023-11-04 14:07:25 +0900

    [level unrated] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,9 @@
+import math
+
+def solution(denum1, num1, denum2, num2):
+    boonmo = num1 * num2
+    boonja = denum1 * num2 + denum2 * num1
+    
+    gcd_value = math.gcd(boonmo, boonja)
+
+    return [boonja / gcd_value, boonmo / gcd_value]
